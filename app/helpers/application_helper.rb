0e139ec26b7ed9779ba3f822df22548ebb5cce8d
an-lee <an.lee.work@gmail.com>
2018-07-06 22:25:27 +0800

    basic ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def body_class
+    return format('%s-page', params[:id].tr('_', '-')) if controller_path == 'pages' && action_name == 'show'
+    format('%s-%s-page', controller_path.tr('/_', '-'), action_name)
+  end
 end
