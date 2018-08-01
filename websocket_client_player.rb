9a46e38b20566296fdc64535a6b0cc8d61645418
an-lee <an.lee.work@gmail.com>
2018-08-01 23:17:15 +0800

    decode/encode msg

--- websocket_client_player.rb
+++ websocket_client_player.rb
@@ -2,6 +2,7 @@ require 'faye/websocket'
 require 'eventmachine'
 require 'json'
 require 'byebug'
+require 'zlib'
 
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJ1aWQiOiIwNTA4YTExNi0xMjM5LTRlMjgtYjE1MC04NWE4ZTNlNmI0MDAiLCJzaWQiOiIxOTE0MzM3ZS1kYTEwLTRiMzAtODM5MC1hNmNjNTIwMWQ3MjYiLCJpYXQiOjE1MzMxMDc4MjEsImV4cCI6MTUzMzE5NDIyMSwianRpIjoiNDY1YjUxZjAtMmExOC00YTFmLWI4ODctMWQ2YzMyMDE5NjUyIiwic2lnIjoiZjgzN2ViZWI0YzI5ODFhZDljNjRiNzBkMGIwZDE4NzI0Yzc2YzYwNzgxYjllNGJmN2E1ODI1MTBkZjdhZGEzOSJ9.QOCum6h7Vkp6nrUsp7zLjshETwdJYwYL5wS3lU9x6HlonWRy2t3UT1MPQ_ekG-K7aaQ-D1OKjqcMpHY4yjj8rFXxfIQ6Lus1rdYY2C1WLo8vn209FkfHbrEXptDQIhiIxN1WaYXFjWvrDxISRdd4Fi3X7gWUXZ-nKLcObTgdvRo"
 
@@ -15,16 +16,26 @@ EM.run {
   ws.on :open do |event|
     p [:open]
 
-    ws.send({
+    msg = {
       "id": SecureRandom.uuid,
       "action": "LIST_PENDING_MESSAGES"
-    }.to_json)
+    }
+    io = StringIO.new 'wb'
+    gzip = Zlib::GzipWriter.new io
+    gzip.write msg.to_json
+    gzip.close
+    data = io.string.unpack('c*')
+    ws.send(data)
   end
 
   ws.on :message do |event|
     p [:message]
-    msg = event.data
-    byebug
+    data = event.data
+    io = StringIO.new(data.pack('c*'), 'rb')
+    gzip = Zlib::GzipReader.new io
+    msg = gzip.read
+    gzip.close
+    p JSON.parse msg
   end
 
   ws.on :error do |event|
